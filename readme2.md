67f7ff1f919a51f8a0d49944594edb9074783002
Federico <fedegomezavalos@gmail.com>
2021-06-19 01:41:20 -0300

    need more css

--- readme2.md
+++ readme2.md
@@ -8,6 +8,10 @@ Desarrollos: Tengo varios desarrollos, la mayoría fue para aprender una técnol
 
 Por qué?: Porque quiero crecer profesionalmente y por lo que estuve viendo, leyendo y me comentaron es posible hacerlo mediante sus capacitaciones. Llegué a Hackademy por Gabriela Martinez que me lo recomendó. 
 
+Área: Desarrollo Mobile
+
+Beca: Deseo aplicar a la beca 100%
+
 Links a ejercicios:
  https://replit.com/@FedeGA93/Most-pople-alive
  https://replit.com/@FedeGA93/Print-thousand-times
--- webApp/index.js
+++ webApp/index.js
@@ -1,22 +1,49 @@
 const URL = "https://60c00381b8d36700175540ff.mockapi.io/api/v1/pets";
 const addPet = document.getElementById("addPet");
-const showPet = document.getElementById("showPet");
+const showPets = document.getElementById("showPet");
 const submitPet = document.getElementById("submitPet");
-const form = document.getElementById("formTest");
+const formDiv = document.getElementById("formDiv");
+const formData = document.getElementById("formData");
+const listDiv = document.getElementById("listDiv");
+let state = 0;
+
+showPets.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchDataFromApi();
+  formDiv.classList.add("hide");
+  state = 0;
+  listDiv.classList.remove("hide");
+
+});
+
+addPet.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (state === 0) {
+    formDiv.classList.remove("hide");
+    listDiv.classList.add("hide");
+    state = 1;
+  } else if (state === 1) {
+    formDiv.classList.add("hide");
+    listDiv.classList.remove("hide");
+    state = 0;
+  }
+});
 
 const fetchDataFromApi = async () => {
   await fetch(URL)
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("displayDiv").appendChild(data);
+      console.log(data);
+      for (const pet of data) {
+        card(pet);
+      }
     });
 };
 
-document.getElementById("submitPet").addEventListener("click", (e) => {
+submitPet.addEventListener("click", (e) => {
   e.preventDefault();
   postData();
 });
-
 
 const postData = async () => {
   const petName = document.getElementById("petName").value;
@@ -37,5 +64,25 @@ const postData = async () => {
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
-  }).then((response) => console.log(response));
+  }).then((response) => {
+    console.log(response.status);
+    if (response.status === 201) {
+      formData.reset();
+      formDiv.classList.add("hide");
+      state = 0;
+    }
+  });
+};
+
+const card = async ({ id, petRace, petName, petAge, petPhoto }) => {
+  const cardTest = await document.createElement("div");
+  cardTest.id = id;
+  cardTest.classList.add("cardColor");
+  const content = `<p> Pet Name: ${petName}</p> 
+                   <p>Pet Age: ${petAge}</p>
+                   <p>Pet Race: ${petRace}</p>
+                   <img src="${petPhoto}" alt="${petName}" width="200" heigh="200" > `;
+  listDiv.appendChild(cardTest);
+  const newDiv = await document.getElementById(id);
+  newDiv.innerHTML = newDiv.innerHTML + content;
 };
